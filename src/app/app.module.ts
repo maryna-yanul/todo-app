4e7bf609ee45ede9884c122ec9e2c811c8131176
Dmytro Paniontko <dmytro.panontko@keenethics.com>
2018-08-21 20:51:27 +0300

    Implemented sign in/up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import * as firebase from 'firebase';
 
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { SignModule } from './sign/sign.module';
 
 @NgModule({
   declarations: [
@@ -11,9 +15,24 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    AppRoutingModule,
+    SignModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    const config = {
+      apiKey: 'AIzaSyA2XW3sd7BVLBhW1QK9inYRoqfVTNNm9Vw',
+      authDomain: 'todo-list-1909.firebaseapp.com',
+      databaseURL: 'https://todo-list-1909.firebaseio.com',
+      projectId: 'todo-list-1909',
+      storageBucket: 'todo-list-1909.appspot.com',
+      messagingSenderId: '43851990378'
+    };
+    firebase.initializeApp(config);
+  }
+}
