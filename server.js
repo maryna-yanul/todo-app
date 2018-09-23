a71a92bdca68b03c999491f4e3180f89f27454d2
Dmytro Paniontko <dmytro.panontko@keenethics.com>
2018-09-23 17:08:23 +0300

    Fix pwa

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const path = require('path');
-const enforce = require('express-sslify');
+const sslRedirect = require('heroku-ssl-redirect');
 
 const app = express();
 
-app.use(enforce.HTTPS());
+app.use(sslRedirect());
 app.use(express.static('./dist/todoApp'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/todoApp/index.html'));
+app.get('/*', function(req,res) {  
+  res.sendFile(path.join(__dirname,'/dist/todoApp/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
