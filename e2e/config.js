d9c81b5900206e0da5d2d65bc3ce1fcc39643c77
Dmytro Paniontko <dmytro.panontko@keenethics.com>
2018-09-28 20:16:19 +0300

    Configurate CircleCI

--- e2e/config.js
+++ e2e/config.js
@@ -1,7 +1,5 @@
-const isProduction = process.env.NODE_ENV === 'production'
-
 module.exports = {
-  url: isProduction ? '' : 'http://localhost:4200',
+  url: 'http://localhost:4200',
   user: {
     email: 'dmytro.panontko@keenethics.com',
     password: '12345678'
--- e2e/index.js
+++ e2e/index.js
@@ -1,14 +1,22 @@
 const puppeteer = require('puppeteer');
 const config = require('./config');
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(`${config.url}/sign/in`);
-  await page.waitFor('input[name="email"]')
+function delay(timeout) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+}
 
-  await page.type('input[name="email"]', config.user.email),
-  await page.type('input[name="password"]', config.user.password)
+(async () => {
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  const page = await browser.newPage();
+  await page.goto(config.url);
+  await delay(4000)
+  await page.goto(`${config.url}/sign/in`);
+  await page.waitFor('input[name="email"]');
+
+  await page.type('input[name="email"]', config.user.email);
+  await page.type('input[name="password"]', config.user.password);
   const navigationPromise = page.waitForNavigation();
   page.click('.sign-in__submit-btn')
 
